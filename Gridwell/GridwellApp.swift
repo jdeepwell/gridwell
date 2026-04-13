c64fb98b287b26bcfeba7b457e83fc27aafba0eb
Johannes Tiefenbrunner <johannes.tiefenbrunner@kencube.com>
2026-04-13 09:30:15 +0200

    Initial Commit

---
+++ Gridwell/GridwellApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridwellApp.swift
+//  Gridwell
+//
+//  Created by Johannes Tiefenbrunner on 13.04.26.
+//
+
+import SwiftUI
+
+@main
+struct GridwellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
